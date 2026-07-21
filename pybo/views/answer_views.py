2941a5d1044bfdb49a1f0e5a507414a5686a430d
isakmama-lab <isakmama@gmail.com>
2026-07-21 17:46:49 +0900

    2026년 7월 21일 질문 등록 기능 구현 및 폼(Form) 모듈 기반 데이터 검증

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from flask import Blueprint, url_for, request, redirect
+from flask import Blueprint, url_for, request, redirect, render_template
 from pybo import db
 from pybo.models import Question, Answer
+from pybo.forms import AnswerForm   # 폼 모듈 임포트
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
 
@@ -10,8 +11,11 @@ def create(question_id):
     # 질문번호를 Question테이블에서 조회
     # 있으면 아래코드를 처리 없으면 404에러
     question = Question.query.get_or_404(question_id)
-    content = request.form['content']
-    answer = Answer(content=content, create_date=datetime.now())
-    question.answer_set.append(answer)
-    db.session.commit()
-    return redirect(url_for('question.detail', question_id=question_id))
+    form = AnswerForm()
+    if form.validate_on_submit():
+        content = request.form['content']
+        answer = Answer(content=content, create_date=datetime.now())
+        question.answer_set.append(answer)
+        db.session.commit()
+        return redirect(url_for('question.detail', question_id=question_id))
+    return render_template('question/question_detail.html', question=question, form=form)
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect, request
 from pybo.models import Question
+from datetime import datetime
+from pybo import db
+from pybo.forms import QuestionForm, AnswerForm
+
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
@@ -10,5 +14,17 @@ def _list():
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()     # 상세 조회 라우터 내부에서 빈 답변 폼 생성
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question)
+    return render_template('question/question_detail.html', question=question, form=form)
+
+# 질문 등록 라우트 함수 추가
+@bp.route('/create/', methods=('GET', 'POST'))
+def create():
+    form = QuestionForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('question._list'))
+    return render_template('question/question_form.html', form=form)
