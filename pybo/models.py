c974e08b82ea271b65014518ea2b546ae2ec2467
isakmama-lab <isakmama@gmail.com>
2026-07-28 16:28:54 +0900

    2026년 7월 28일 질문 추천 완성

--- pybo/models.py
+++ pybo/models.py
@@ -1,4 +1,20 @@
 from pybo import db
+from sqlalchemy import Table
+
+# 중간 테이블 정의
+question_voter = Table(
+    'question_voter',
+    db.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('question_id', db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'), primary_key=True)
+)
+
+answer_voter = Table(
+    'answer_voter',
+    db.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('answer_id', db.Integer, db.ForeignKey('answer.id', ondelete='CASCADE'), primary_key=True)
+)
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +26,13 @@ class Question(db.Model):
     # user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=True, server_default='1')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('question_set'))
+    # 추천인 (다대다)
+    voter = db.relationship(
+        'User',                     # 추천한 사용자(User) 모델과 연결
+        secondary=question_voter,   # 다대다 관계를 위한 중간 테이블(question_voter) 사용
+        backref=db.backref('question_voter_set',   # User 객체에서 추천한 질문 목록을 조회하는 속성
+                            lazy='dynamic'         # 실제 조회 시점에 SQL을 실행하여 Query 객체를 반환
+                            ))
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +44,9 @@ class Answer(db.Model):
     # user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=True, server_default='1')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('answer_set'))
+
+    voter = db.relationship('User', secondary=answer_voter,
+                            backref=db.backref('answer_voter_set', lazy='dynamic'))
                             
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
