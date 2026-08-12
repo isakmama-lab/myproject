0cd136de491ec79198bd6a5814ecc865335f1084
isakmama-lab <isakmama@gmail.com>
2026-08-12 20:26:40 +0900

    gitignore 수정(config 포함)

--- config.py
+++ config.py
@@ -8,5 +8,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # CSRF 토큰 생성을 위한 비밀키 추가
 # SECRET_KEY = "dev"
 
-# Render 환경 변수에서 SECRET_KEY를 가져오고, 없으면 기본값 'dev' 사용
+# 배포시 Render 환경 변수에서 SECRET_KEY를 가져오고, 
+# 없으면 기본값 'dev' 사용
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
